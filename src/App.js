cb124b4c1f1e75c1df89b327131ae34a0c67a43c
Adam Rashdan <ar223hf@student.lnu.se>
2020-04-26 10:43:45 +0200

    modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import './App.css';
-import CovidTest from "./components/HomePage/CovidTest/CovidTest";
-import Navbar from "./components/HomePage/Navbar/Navbar";
-import Register from "./components/HomePage/Register/Register";
-import Signin from "./components/HomePage/Signin/Signin";
-import LandingPage from "./components/HomePage/LandingPage/LandingPage";
+import CovidTest from "./components/CovidTest/CovidTest";
+import Navbar from "./components/Navbar/Navbar";
+import Register from "./components/Register/Register";
+import Signin from "./components/Signin/Signin";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 
 
